7d4b05a6de053481d1ce45fb487d6c64c6beb14b
Tim Potze <timpotze@gmail.com>
2014-03-31 00:46:43 +0200

    Build binary

--- source/SanMap/SanMap/Properties/AssemblyInfo.cs
+++ source/SanMap/SanMap/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SanMap")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cut images up in multiple part for use with Google Maps.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SanMap")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © Tim Potze 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
