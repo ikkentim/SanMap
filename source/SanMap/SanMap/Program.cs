c6d5c3a9f8fe6bf085317ad0ee74e34fd1f34aec
Tim Potze <timpotze@gmail.com>
2014-03-31 00:44:58 +0200

    Added Cutting tool logic

--- source/SanMap/SanMap/Program.cs
+++ source/SanMap/SanMap/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
@@ -8,14 +10,21 @@ namespace SanMap
     {
         private static void Main(string[] args)
         {
-            string inputPath =null;
+            //Options
+            string baseName = null;
+            string inputPath = null;
             string outputPath = null;
+            string outputExtension = "png";
+            ImageFormat outputFormat;
             int minimumZoomLevel = 0;
-            int maximumZoomLevel = 2;
+            int maximumZoomLevel = 4;
+            int targetSize = 512;
             bool showHelp = false;
 
             #region Args checking
-            var p = new OptionSet
+
+            //Declare options.
+            var optionSet = new OptionSet
             {
                 {
                     "i|input=",
@@ -24,18 +33,28 @@ namespace SanMap
                 },
                 {
                     "o|output=",
-                    "the output path",
+                    "the output path (Default: same path as input image)",
                     (value) => outputPath = value
                 },
                 {
                     "z|zoom|maximum-zoom=",
-                    "the maximum zoom level",
+                    "the maximum zoom level (Default: 4)",
                     (int value) => maximumZoomLevel = value
                 },
                 {
                     "minimum-zoom=",
-                    "the minimum zoom level",
+                    "the minimum zoom level (Default: 0)",
                     (int value) => minimumZoomLevel = value < 0 ? 0 : value
+                },
+                {
+                    "s|size|target-size=",
+                    "the resulting with/height (Default: 512)",
+                    (int value) => targetSize = value < 8 ? 8 : value
+                },
+                {
+                    "e|extension|output-extension=",
+                    "the output extension (Default: png)",
+                    (value) => outputExtension = value
                 },
                 {
                     "h|help", "show this message and exit",
@@ -43,9 +62,10 @@ namespace SanMap
                 },
             };
 
+            //Try to parse the optionset, or print an error.
             try
             {
-                p.Parse(args);
+                optionSet.Parse(args);
             }
             catch (OptionException e)
             {
@@ -55,12 +75,14 @@ namespace SanMap
                 return;
             }
 
+            //Show help if asked for.
             if (showHelp)
             {
-                ShowHelp(p);
+                ShowHelp(optionSet);
                 return;
             }
 
+            //Check if inputPath is given.
             if (inputPath == null)
             {
                 Console.Write("SanMap: ");
@@ -69,6 +91,7 @@ namespace SanMap
                 return;
             }
 
+            //Check if inputPath exists.
             if (!File.Exists(inputPath))
             {
                 Console.Write("SanMap: ");
@@ -77,7 +100,8 @@ namespace SanMap
                 return;
             }
 
-            if(!new []{".bmp", ".png", ".jpg", ".gif"}.Contains(Path.GetExtension(inputPath)))
+            //Check if inputPath is an supported ImageFormat.
+            if (!new[] {".bmp", ".png", ".jpg", ".gif"}.Contains(Path.GetExtension(inputPath)))
             {
                 Console.Write("SanMap: ");
                 Console.WriteLine("Input file is no known image format.");
@@ -85,27 +109,144 @@ namespace SanMap
                 return;
             }
 
+            //Check if outputExtension is an supported ImageFormat.
+            if (!new[] {"bmp", "png", "jpg", "gif"}.Contains(outputExtension))
+            {
+                Console.Write("SanMap: ");
+                Console.WriteLine("Output extension is not known .");
+                Console.WriteLine("Try 'SanMap --help' for more information.");
+                return;
+            }
+
+            //If no outputpath is given, generate one.
             if (outputPath == null)
                 outputPath = Path.GetDirectoryName(inputPath);
 
+            //Calculate basename of output images.
+            baseName = Path.GetFileNameWithoutExtension(inputPath);
+
+            //Create directory of outputPath.
             Directory.CreateDirectory(outputPath);
+
+            //Set outputFormat according to outputExtension.
+            switch (outputExtension)
+            {
+                case "bmp":
+                    outputFormat = ImageFormat.Bmp;
+                    break;
+                case "jpg":
+                    outputFormat = ImageFormat.Jpeg;
+                    break;
+                case "gif":
+                    outputFormat = ImageFormat.Gif;
+                    break;
+                default:
+                    outputFormat = ImageFormat.Png;
+                    break;
+            }
 
             #endregion
 
-            //Info
+            #region Show information
+
+            //Show entered information
             Console.WriteLine("SanMap cutting tool...");
             Console.WriteLine("Input file: " + inputPath);
             Console.WriteLine("Output directory: " + outputPath);
+            Console.WriteLine("Target size: " + targetSize + "x" + targetSize);
             Console.WriteLine("Zoom levels: " + minimumZoomLevel + "-" + maximumZoomLevel);
+
+            #endregion
+
+            #region Cutting
+
+
+            try
+            {
+                //Read the input image to memory.
+                var baseImage = Image.FromFile(inputPath) as Bitmap;
+
+                //Loop trough every zoom level.
+                for (var zoom = minimumZoomLevel; zoom <= maximumZoomLevel; zoom++)
+                {
+                    //Show our progress
+                    Console.Write("Processing zoom level " + zoom);
+
+                    //Caclucate the number of tiles we're processing in this zoom level.
+                    var tiles = 1 << zoom;
+
+                    //Calculate the source-tilesize
+                    var tileWidth = baseImage.Width/tiles;
+                    var tileHeight = baseImage.Height/tiles;
+
+                    //Keep track of our progress
+                    var progress = 0;
+
+                    //Loop trough every tile X/Y
+                    for (var tileX = 0; tileX < tiles; tileX++)
+                        for (var tileY = 0; tileY < tiles; tileY++)
+                        {
+                            //Create a new bitmap of the source-tilesize
+                            var baseTile = new Bitmap(tileWidth, tileHeight);
+                            using (Graphics g = Graphics.FromImage(baseTile))
+                            {
+                                //Copy the image from the source
+                                g.DrawImage(baseImage, new Rectangle(0, 0, tileWidth, tileHeight),
+                                    new Rectangle(tileWidth*tileX, tileHeight*tileY, tileWidth, tileHeight),
+                                    GraphicsUnit.Pixel);
+                            }
+
+                            //Resize the tile
+                            var tile = new Bitmap(baseTile, new Size(targetSize, targetSize));
+
+                            //Generate the output filename
+                            var outputFile = string.Format("{0}.{1}.{2}.{3}.{4}", baseName, zoom, tileX, tileY,
+                                outputExtension);
+
+                            //Save the tile
+                            tile.Save(Path.Combine(outputPath, outputFile), outputFormat);
+
+                            //Dispose tile
+                            baseTile.Dispose();
+                            tile.Dispose();
+
+                            //Update current progress
+                            var currentProgress = ((tileX*tiles + tileY + 1)*5)/(tiles*tiles);
+                            while (progress < currentProgress)
+                            {
+                                progress++;
+                                Console.Write(".");
+                            }
+                        }
+
+                    //End line of 'Processing zoom level X.....'
+                    Console.WriteLine();
+                }
+            }
+            catch (Exception e)
+            {
+                //Whoops!
+                Console.WriteLine();
+                Console.WriteLine("ERROR THROWN: ");
+                Console.Write("\t");
+                Console.WriteLine(e.Message);
+            }
+
+            #endregion
         }
 
         static void ShowHelp(OptionSet p)
         {
+            //Show usage, options and supported image formats
             Console.WriteLine("Usage: SanMap [OPTIONS]");
             Console.WriteLine("Cut images up in multiple part for use with Google Maps.");
+
             Console.WriteLine();
             Console.WriteLine("Options:");
             p.WriteOptionDescriptions(Console.Out);
+
+            Console.WriteLine();
+            Console.WriteLine("Supported image types: bmp, png, jpg, gif");
         }
     }
 }
