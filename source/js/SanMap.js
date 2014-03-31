212be2c90741ae9de3bfcd0d909ae0c692427dc7
Tim Potze <timpotze@gmail.com>
2014-03-31 13:57:38 +0200

    Added repeating option, comments, cleanup

--- source/js/SanMap.js
+++ source/js/SanMap.js
@@ -1,83 +1,77 @@
-//SanMap projection
+//Represents the Projection used in SanMap
 function SanMapProjection() {
-    var projectionRange = 256;
+    //The range across the map
+    var projectionRange = 512;
+    //The origin (0,0) of the map (Middle of the map)
     this.pixelOrigin_ = new google.maps.Point(projectionRange / 2, projectionRange / 2);
+    //The number of pixels per longitude degree
     this.pixelsPerLonDegree_ = projectionRange / 360;
-    this.pixelsPerLonRadian_ = projectionRange / (2 * Math.PI);
-    this.scaleLat = 2;
-    this.scaleLng = 2;
-
+    //Method to convert LatLng to a Point.
     this.fromLatLngToPoint = function (latLng, opt_point) {
         var point = opt_point || new google.maps.Point(0, 0);
-        point.x = (this.pixelOrigin_.x + latLng.lng() * this.scaleLng * this.pixelsPerLonDegree_);
-        point.y = (this.pixelOrigin_.y - latLng.lat() * this.scaleLat * this.pixelsPerLonDegree_);
+        point.x = this.pixelOrigin_.x + latLng.lng() * this.pixelsPerLonDegree_;
+        point.y = this.pixelOrigin_.y - latLng.lat() * this.pixelsPerLonDegree_;
         return point;
     }
-
+    //Method to convert Point to LatLng
     this.fromPointToLatLng = function (point) {
-        var lng = (((point.x - this.pixelOrigin_.x) / this.pixelsPerLonDegree_) / this.scaleLng);
-        var lat = ((-( point.y - this.pixelOrigin_.y) / this.pixelsPerLonDegree_) / this.scaleLat);
+        var lng = (point.x - this.pixelOrigin_.x) / this.pixelsPerLonDegree_;
+        var lat = -( point.y - this.pixelOrigin_.y) / this.pixelsPerLonDegree_;
         return new google.maps.LatLng(lat, lng, true);
     }
 };
 
-//Map Type object
+//Represents a MapType used in SanMap
 function SanMapType(minZoom, maxZoom, getTileUrl) {
-    this.getImageMapType = function () {
+    this.getImageMapType = function (repeating) {
         return new google.maps.ImageMapType({
-            getTileUrl: function (coords, zoom) {
-                if (coords.y < 0 || coords.y >= 1 << zoom || coords.x < 0 || coords.x >= 1 << zoom)
+            getTileUrl: function (coord, zoom) {
+                var x = coord.x, y = coord.y, max = 1 << zoom;
+                //If not repeating and x is outside range or y is outside range, return null
+                if (y < 0 || y >= max || (repeating !== true && (x < 0 || x >= max))) {
                     return null;
+                }
+                //Get tileX within range
+                for (; x < 0; x += max);
 
-                return getTileUrl(zoom, coords.x, coords.y);
+                return getTileUrl(zoom, x % max, y);
             },
-            tileSize: new google.maps.Size(256, 256),
-            maxZoom: maxZoom,
-            minZoom: minZoom,
-            opacity: 1.0
+            tileSize: new google.maps.Size(512, 512),//Range of the map
+            maxZoom: maxZoom,//Set zoom levels as given
+            minZoom: minZoom
         });
     }
 };
 
-function SanMap(canvas, mapTypes, zoom, center) {
-    this.canvas = canvas;
-
-    if(mapTypes === undefined) {
+//Represents a SanMap Map.
+function SanMap(canvas, mapTypes, zoom, center, repeating) {
+    //If no mapTypes are parsed, don't continue.
+    if (mapTypes === undefined || mapTypes.length == 0) {
         return;
     }
 
-    var mapTypeNames = new Array();
-    for (var key in mapTypes){
-        if (mapTypes.hasOwnProperty(key)) {
-            mapTypeNames.push(key);
-        }
-    }
-    var mapOptions = {
-        zoom: zoom || 2,
-        center: center || SanMap.getLatLngFromPos(0,0),
-        streetViewControl: false,
-        keyboardShortcuts: false,
-        panControl: false,
+    //Create map with given options
+    this.map = new google.maps.Map(document.getElementById(canvas), {
+        zoom: zoom || 2,//Default zoom level: 2
+        center: center || SanMap.getLatLngFromPos(0, 0),//Default center point: Blueberry
+        streetViewControl: false,//No StreetView in GTA
         mapTypeControlOptions: {
-            mapTypeIds: mapTypeNames
+            mapTypeIds: Object.keys(mapTypes)//Get names from mapTypes keys
         }
-    };
+    });
 
-    this.map = new google.maps.Map(document.getElementById(canvas), mapOptions);
-
-    for (var key in mapTypes){
+    for (var key in mapTypes) {//Iterate trough mapTypes and add them to the map
         if (mapTypes.hasOwnProperty(key)) {
-            var type = mapTypes[key].getImageMapType();
-            type.name=key;
-            type.alt=key;
+            var type = mapTypes[key].getImageMapType(repeating || false);
+            type.name = type.alt = key;//key = name
             type.projection = new SanMapProjection();
             this.map.mapTypes.set(key, type);
         }
     }
 
-    this.map.setMapTypeId(mapTypeNames[0]);
+    this.map.setMapTypeId(Object.keys(mapTypes)[0]);
 };
 
-SanMap.getLatLngFromPos = function(x, y) {
-    return new google.maps.LatLng(y / 3000 * 90, x / 3000 * 90);
+SanMap.getLatLngFromPos = function (x, y) {
+    return new google.maps.LatLng(y / 3000 * 180, x / 3000 * 180);
 }
