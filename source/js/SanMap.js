e4fb97bfda59f35325cb3bc73e3a69a4855a9acd
Tim Potze <timpotze@gmail.com>
2014-03-31 12:18:23 +0200

    Removed test tiles, fixed projection

--- source/js/SanMap.js
+++ source/js/SanMap.js
@@ -1,48 +1,35 @@
+//SanMap projection
+function SanMapProjection() {
+    var projectionRange = 256;
+    this.pixelOrigin_ = new google.maps.Point(projectionRange / 2, projectionRange / 2);
+    this.pixelsPerLonDegree_ = projectionRange / 360;
+    this.pixelsPerLonRadian_ = projectionRange / (2 * Math.PI);
+    this.scaleLat = 2;
+    this.scaleLng = 2;
 
-// Setup the new type of projection
-function EuclideanProjection() {
-    var EUCLIDEAN_RANGE = 256;
-    this.pixelOrigin_ = new google.maps.Point(EUCLIDEAN_RANGE / 2, EUCLIDEAN_RANGE / 2);
-    this.pixelsPerLonDegree_ = EUCLIDEAN_RANGE / 360;
-    this.pixelsPerLonRadian_ = EUCLIDEAN_RANGE / (2 * Math.PI);
-    this.scaleLat = 2;	// Height
-    this.scaleLng = 2;	// Width
-    this.offsetLat = 0;	// Height
-    this.offsetLng = 0;	// Width
+    this.fromLatLngToPoint = function (latLng, opt_point) {
+        var point = opt_point || new google.maps.Point(0, 0);
+        point.x = (this.pixelOrigin_.x + latLng.lng() * this.scaleLng * this.pixelsPerLonDegree_);
+        point.y = (this.pixelOrigin_.y - latLng.lat() * this.scaleLat * this.pixelsPerLonDegree_);
+        return point;
+    }
+
+    this.fromPointToLatLng = function (point) {
+        var lng = (((point.x - this.pixelOrigin_.x) / this.pixelsPerLonDegree_) / this.scaleLng);
+        var lat = ((-( point.y - this.pixelOrigin_.y) / this.pixelsPerLonDegree_) / this.scaleLat);
+        return new google.maps.LatLng(lat, lng, true);
+    }
 };
-
-EuclideanProjection.prototype.fromLatLngToPoint = function (latLng, opt_point) {
-    var point = opt_point || new google.maps.Point(0, 0);
-
-    var origin = this.pixelOrigin_;
-    point.x = (origin.x + (latLng.lng() + this.offsetLng ) * this.scaleLng * this.pixelsPerLonDegree_);
-    point.y = (origin.y + (-1 * latLng.lat() + this.offsetLat ) * this.scaleLat * this.pixelsPerLonDegree_);
-    return point;
-};
-
-EuclideanProjection.prototype.fromPointToLatLng = function (point) {
-    var me = this;
-
-    var origin = me.pixelOrigin_;
-    var lng = (((point.x - origin.x) / me.pixelsPerLonDegree_) / this.scaleLng) - this.offsetLng;
-    var lat = ((-1 * ( point.y - origin.y) / me.pixelsPerLonDegree_) / this.scaleLat) - this.offsetLat;
-    return new google.maps.LatLng(lat, lng, true);
-};
-
 
 //Map Type object
 function SanMapType(minZoom, maxZoom, getTileUrl) {
     this.getImageMapType = function () {
         return new google.maps.ImageMapType({
-            getTileUrl: function (coord, zoom) {
-                if (coord.y < 0 || coord.y >= 1 << zoom || coord.x < 0 || coord.x >= 1 << zoom)
+            getTileUrl: function (coords, zoom) {
+                if (coords.y < 0 || coords.y >= 1 << zoom || coords.x < 0 || coords.x >= 1 << zoom)
                     return null;
 
-                var coordx = (coord.x % (1 << zoom));
-                while (coordx < 0) {
-                    coordx += 1 << zoom;
-                }
-                return getTileUrl(zoom, coordx, coord.y);
+                return getTileUrl(zoom, coords.x, coords.y);
             },
             tileSize: new google.maps.Size(256, 256),
             maxZoom: maxZoom,
@@ -55,18 +42,19 @@ function SanMapType(minZoom, maxZoom, getTileUrl) {
 function SanMap(canvas, mapTypes, zoom, center) {
     this.canvas = canvas;
 
+    if(mapTypes === undefined) {
+        return;
+    }
+
     var mapTypeNames = new Array();
     for (var key in mapTypes){
         if (mapTypes.hasOwnProperty(key)) {
             mapTypeNames.push(key);
-            mapTypes[key].name=key;
-            mapTypes[key].alt=key;
-            mapTypes[key].projection = new EuclideanProjection();
         }
     }
     var mapOptions = {
-        zoom: zoom === undefined ? 2 : zoom,
-        center: SanMap.getLatLngFromPos(0,0),
+        zoom: zoom || 2,
+        center: center || SanMap.getLatLngFromPos(0,0),
         streetViewControl: false,
         keyboardShortcuts: false,
         panControl: false,
@@ -82,6 +70,7 @@ function SanMap(canvas, mapTypes, zoom, center) {
             var type = mapTypes[key].getImageMapType();
             type.name=key;
             type.alt=key;
+            type.projection = new SanMapProjection();
             this.map.mapTypes.set(key, type);
         }
     }
@@ -90,5 +79,5 @@ function SanMap(canvas, mapTypes, zoom, center) {
 };
 
 SanMap.getLatLngFromPos = function(x, y) {
-    return new google.maps.LatLng(x / (2985 + 1) * 90, y / (2985 + 1) * 90);
+    return new google.maps.LatLng(y / 3000 * 90, x / 3000 * 90);
 }
