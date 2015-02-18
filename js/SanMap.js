28e64b0370116390c0f48bbe511e0ebddbfca1ec
Tim Potze <timpotze@gmail.com>
2015-02-18 17:04:25 +0100

    Updated SanMap.js

--- js/SanMap.js
+++ js/SanMap.js
@@ -1,22 +1,7 @@
 // SanMap.js
 // Tool for drawing Google Maps of San Andreas.
 // Written by Tim Potze
-//
-// This is free and unencumbered software released into the public domain.
-//
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
-//
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
-//
+
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
 // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
@@ -28,88 +13,189 @@
 // For more information, please refer to <http://unlicense.org>
 //
 
-//Represents the Projection used in SanMap
-function SanMapProjection() {
-    //The range across the map
-    var projectionRange = 512;
-    //The origin (0,0) of the map (Middle of the map)
-    this.pixelOrigin_ = new google.maps.Point(projectionRange / 2, projectionRange / 2);
-    //The number of pixels per longitude degree
-    this.pixelsPerLonDegree_ = projectionRange / 360;
-    this.scaleLat = 2;	// latitude scale
-    this.scaleLng = 2;	// longitude scale
-    //Method to convert LatLng to a Point.
+/* Create a set of helper classes.
+ */
+
+/** 
+ * Projection specialized for San Andreas, based on GallPetersProjection
+ * available at:
+ * https://developers.google.com/maps/documentation/javascript/examples/map-projection-simple
+ * @class SanMapProjection
+ * @constructor
+ * *implements {google.maps.Projection}
+ */
+function SanMapProjection(tileSize) {
+	/**
+     * The range across the map.
+	 *
+	 * @property projectionRange_
+	 * @type {Number}
+	 */
+    var projectionRange_ = tileSize;
+	
+	/**
+     * The origin of the map.
+	 *
+	 * @property pixelOrigin_
+	 * @type {Object}
+	 */
+    this.pixelOrigin_ = new google.maps.Point(projectionRange_ / 2, 
+		projectionRange_ / 2);
+	
+	/**
+     * The number of pixels per longitude degree.
+	 *
+	 * @property pixelsPerLonDegree_
+	 * @type {Number}
+	 */
+    this.pixelsPerLonDegree_ = projectionRange_ / 360;
+	
+	/**
+     * Converts a google.maps.LatLng to google.maps.Point.
+	 *
+	 * @method fromLatLngToPoint
+	 * @param {Object} latLng The LatLng object to convert.
+	 * @param {Object} opt_point optional point type to use as return type
+	 *	instead of google.maps.Point.
+	 * @return {Object} The newly created point.
+	 */
     this.fromLatLngToPoint = function (latLng, opt_point) {
         var point = opt_point || new google.maps.Point(0, 0);
-        point.x = this.pixelOrigin_.x + latLng.lng() * this.pixelsPerLonDegree_ * this.scaleLng;
-        point.y = this.pixelOrigin_.y - latLng.lat() * this.pixelsPerLonDegree_ * this.scaleLat;
+		
+        point.x = this.pixelOrigin_.x + latLng.lng() *
+			this.pixelsPerLonDegree_ * 2;
+        point.y = this.pixelOrigin_.y - latLng.lat() *
+			this.pixelsPerLonDegree_ * 2;
+			
         return point;
     }
-    //Method to convert Point to LatLng
+	
+	/**
+     * Converts a google.maps.Point to google.maps.LatLng.
+	 *
+	 * @method fromLatLngToPoint
+	 * @param {Object} point The Point object to convert.
+	 * @return {Object} The newly created LatLng.
+	 */
     this.fromPointToLatLng = function (point) {
-        var lng = (point.x - this.pixelOrigin_.x) / this.pixelsPerLonDegree_ / this.scaleLng;
-        var lat = (-point.y + this.pixelOrigin_.y) / this.pixelsPerLonDegree_ / this.scaleLat;
+        var lng = (point.x - this.pixelOrigin_.x) /
+			this.pixelsPerLonDegree_ / 2;
+        var lat = (-point.y + this.pixelOrigin_.y) /
+			this.pixelsPerLonDegree_ / 2;
+			
         return new google.maps.LatLng(lat, lng, true);
     }
 };
 
-//Represents a MapType used in SanMap
-function SanMapType(minZoom, maxZoom, getTileUrl) {
+/**
+ * Simple class for providing a google.maps.ImageMapType based on the provided
+ * zoom limitations and function for providing tiles.
+ * @class SanMapType
+ * @constructor
+ */
+function SanMapType(minZoom, maxZoom, getTileUrl, tileSize) {
+
+	/**
+	 * Creates an instance of google.maps.ImageMapType based on the provided
+	 * zoom limitations and function for providing tiles.
+	 *
+	 * @method getImageMapType
+	 * @param {Boolean} repeating Whether the map should repeat horizontally.
+	 * @return {Object} The newly created ImageMapType.
+	 */
     this.getImageMapType = function (repeating) {
+		/* Default tileSize to 512.
+		 */
+		tileSize = tileSize || 512;
+		
         return new google.maps.ImageMapType({
             getTileUrl: function (coord, zoom) {
-                var x = coord.x, y = coord.y, max = 1 << zoom;
-                //If not repeating and x is outside range or y is outside range, return null
-                if (y < 0 || y >= max || (repeating !== true && (x < 0 || x >= max))) {
+                var x = coord.x, 
+					y = coord.y, 
+					max = 1 << zoom;
+
+				/* If not repeating and x is outside of the range -or- y is
+				 * outside of the range, return a clear tile. This can be
+				 * provided by getTileUrl, using the tile coordinates (-1, -1).
+				 */
+                if (y < 0 || y >= max || 
+					(repeating !== true && (x < 0 || x >= max))) {
                     return getTileUrl(zoom, -1, -1);
                 }
-                //Get tileX within range
-                for (; x < 0; x += max);
-
-                return getTileUrl(zoom, x % max, y);
+				
+				/*
+				 * Return the provided tile. Make sure x is within the 
+				 * range 0 - max.
+				 */
+                return getTileUrl(zoom, (((x % max) + max) % max), y);
             },
-            tileSize: new google.maps.Size(512, 512),//Range of the map
-            maxZoom: maxZoom,//Set zoom levels as given
+            tileSize: new google.maps.Size(tileSize, tileSize),
+            maxZoom: maxZoom,
             minZoom: minZoom
         });
     }
 };
 
-//Represents a SanMap Map.
-function SanMap(canvas, mapTypes, zoom, center, repeating) {
-    //If no mapTypes are parsed, don't continue.
+/* Define a number of SanMap methods.
+ */
+function SanMap(){ }
+
+/**
+ * Creates an instance of google.maps.Map with the provided map types.
+ *
+ * @method createMap
+ * @param {Object} canvas The element to draw the map on.
+ * @param {Number} mapTypes The map types available in the map control.
+ * @param {Number} zoom The initial zoom level.
+ * @param {Object} center The intial center point to focus on.
+ * @param {Boolean} repeating Whether the map should repeat horizontally.
+ * @param {String} defaultMap The default map type.
+ * @return {Object} The newly created Map.
+ */
+SanMap.createMap = function(canvas, mapTypes, zoom, center, repeating, 
+	defaultMap) {
+	/* If no mapTypes are parsed, return null and display a warning
+	 */
     if (mapTypes === undefined || mapTypes.length == 0) {
-        return;
+		console.warn("SanMap: No map types were parsed with SanMap.createMap.");
+        return null;
     }
 
-    //Create map with given options
-    this.map = new google.maps.Map(document.getElementById(canvas), {
-        zoom: zoom || 2,//Default zoom level: 2
-        center: center || SanMap.getLatLngFromPos(0, 0),//Default center point: Blueberry
-        streetViewControl: false,//No StreetView in GTA
+	/* Create the map
+	 */
+    var map = new google.maps.Map(canvas,  {
+        zoom: zoom || 2,
+        center: center || SanMap.getLatLngFromPos(0, 0),
+        streetViewControl: false,
         mapTypeControlOptions: {
-            mapTypeIds: Object.keys(mapTypes)//Get names from mapTypes keys
+            mapTypeIds: Object.keys(mapTypes)
         }
     });
 
-    for (var key in mapTypes) {//Iterate trough mapTypes and add them to the map
+	/* Add every map type to the map.
+	 */
+    for (var key in mapTypes) {
         if (mapTypes.hasOwnProperty(key)) {
             var type = mapTypes[key].getImageMapType(repeating || false);
-            type.name = type.alt = key;//key = name
-            type.projection = new SanMapProjection();
-            this.map.mapTypes.set(key, type);
+            type.name = type.alt = key;
+			type.projection = new SanMapProjection(type.tileSize.width);
+            map.mapTypes.set(key, type);
         }
     }
 
-    //Set default mapType to first in mayTypes array.
-    this.map.setMapTypeId(Object.keys(mapTypes)[0]);
+	/* Set the default map type.
+	 */
+    map.setMapTypeId(defaultMap || Object.keys(mapTypes)[0]);
 
-    //If not repeating, bound the viewable area
+	/* If not repeating, bound the viewable area.
+	 */
     if (!repeating) {
-        var map = this.map,
-            bounds = new google.maps.LatLngBounds(new google.maps.LatLng(-90,-90), new google.maps.LatLng(90,90));
+        bounds = new google.maps.LatLngBounds(new google.maps.LatLng(-90,-90), 
+			new google.maps.LatLng(90,90));
 
-        //When center changed, check if it's within the bounds of the map
+		/* When the center changes, check if the new center is within the bounds
+		 * of the map. If not, move the center to within these bounds.
+		 */
         google.maps.event.addListener(map, 'center_changed', function () {
             if (bounds.contains(map.getCenter()))
                 return;
@@ -117,24 +203,46 @@ function SanMap(canvas, mapTypes, zoom, center, repeating) {
             var lng = map.getCenter().lng(),
                 lat = map.getCenter().lat();
 
-            //Check if any direction passed bounds
-            if (lng < bounds.getSouthWest().lng()) lng = bounds.getSouthWest().lng();
-            if (lng > bounds.getNorthEast().lng()) lng = bounds.getNorthEast().lng();
-            if (lat < bounds.getSouthWest().lat()) lat = bounds.getSouthWest().lat();
-            if (lat > bounds.getNorthEast().lat()) lat = bounds.getNorthEast().lat();
+            if (lng < bounds.getSouthWest().lng())
+				lng = bounds.getSouthWest().lng();
+				
+            if (lng > bounds.getNorthEast().lng())
+				lng = bounds.getNorthEast().lng();
+				
+            if (lat < bounds.getSouthWest().lat())
+				lat = bounds.getSouthWest().lat();
+				
+            if (lat > bounds.getNorthEast().lat())
+				lat = bounds.getNorthEast().lat();
+				
             map.setCenter(new google.maps.LatLng(lat, lng));
         });
     }
+	
+	return map;
 };
 
-//Method to convert GTA positions to google.maps.LatLng objects
+/**
+ * Converts a GTA:SA coordinates to an instance of google.maps.LatLng.
+ *
+ * @method getLatLngFromPos
+ * @param {Number} x The GTA:SA x-coordinate.
+ * @param {Number} y The GTA:SA y-coordinate.
+ * @return {Object} The newly created LatLng.
+ */
 SanMap.getLatLngFromPos = function (x, y) {
     return typeof(x) == "object" 
 		? new google.maps.LatLng(x.y / 3000 * 90, x.x / 3000 * 90) 
 		: new google.maps.LatLng(y / 3000 * 90, x / 3000 * 90);
 }
 
-//Method to convert google.maps.LatLng objects to GTA positions
+/**
+ * Converts an instance of google.maps.LatLng to a GTA:SA coordinates.
+ *
+ * @method getPosFromLatLng
+ * @param {Object} latLng The LatLng to convert..
+ * @return {Object} An Object containing the GTA:SA coordinates.
+ */
 SanMap.getPosFromLatLng = function (latLng) {
     return {x: latLng.lng() * 3000 / 90, y: latLng.lat() * 3000 / 90};
 }
